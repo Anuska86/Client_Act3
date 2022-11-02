e280a0f3dad0d4ca05137cc31e96c9d2a8ba0190
Anuska86 <anasrm86@gmail.com>
2022-11-02 10:32:19 +0100

    act3falseforms

--- actividad03.js
+++ actividad03.js
@@ -4,18 +4,18 @@ class Extra {
     this.url = null;
   }
   getHTML() {
-    return `<span><img src='${this.url}'/>${this.price}</span>`;
+    return `<span><img class="image" src='${this.url}' />${this.price}</span>`;
   }
 }
 
 let extra = new Extra();
 extra.price = "17€";
-extra.url = "./imgs/kart.jpg";
+extra.url = "./imgs/banana.jpg";
 console.log(extra.getHTML());
 
 let extra2 = new Extra();
 extra2.price = "18€";
-extra2.url = "./imgs/luigi.jpg";
+extra2.url = "./imgs/wheels.jpg";
 console.log(extra2.getHTML());
 
 class Coche {
@@ -24,17 +24,17 @@ class Coche {
     this.speed = null;
     this.extras = [];
   }
-  addExtra(extra){
+  addExtra(extra) {
     this.extras.push(extra);
   }
-  getHTML(){
+  getHTML() {
     let stringIni = `<span>${this.name} ${this.speed} [`;
     let stringMid = "";
     for (let i = 0; i < this.extras.length; i++) {
-        stringMid += this.extras[i].getHTML() + " ";
-      }
+      stringMid += this.extras[i].getHTML() + " ";
+    }
     let stringEnd = `]</span><br/>`;
-    let returnString = [stringIni,stringMid,stringEnd].join(' ');
+    let returnString = [stringIni, stringMid, stringEnd].join(" ");
     return returnString;
   }
 }
@@ -46,25 +46,65 @@ coche.addExtra(extra);
 coche.addExtra(extra2);
 console.log(coche.getHTML());
 
-let extraDisponibles=[extra,extra2];
+let extraDisponibles = [extra, extra2];
 
-function mostrarExtras(){
-    let result = "";
-    for (let i = 0; i < extraDisponibles.length; i++) {
-        result += extraDisponibles[i].getHTML() + " ";
-      }
-    return result;
+function mostrarExtras() {
+  let result = "";
+  for (let i = 0; i < extraDisponibles.length; i++) {
+    result += extraDisponibles[i].getHTML() + " ";
+  }
+  return result;
 }
 
-let cochesDisponibles=new Array();
-cochesDisponibles[coche.name]=coche;
+let cochesDisponibles = new Array();
+cochesDisponibles[coche.name] = coche;
 console.log(cochesDisponibles);
 
-function mostrarCoches(){
-    let result = "";
-    let keys = Object.keys(cochesDisponibles);
-    for (let i = 0; i < keys.length; i++) {
-        result += cochesDisponibles[keys[i]].getHTML() + " ";
-      }
-    return result;
+function mostrarCoches() {
+  let result = "";
+  let keys = Object.keys(cochesDisponibles);
+  for (let i = 0; i < keys.length; i++) {
+    result += cochesDisponibles[keys[i]].getHTML() + " ";
+  }
+  return result;
+}
+
+function addExtras() {
+  let extra = new Extra();
+  extra.price = document.getElementById("price").value;
+  extra.url = "./imgs/" + document.getElementById("images").value + ".jpg";
+
+  extraDisponibles.push(extra);
+  mostrarExtras();
+  console.log(extraDisponibles);
+}
+
+function addCar(){
+  let coche = new Coche();
+  coche.name =document.getElementById("name").value;
+  coche.speed = document.getElementById("velocity").value;
+
+  cochesDisponibles[coche.name]=coche;
+  mostrarCoches();
+  console.log(cochesDisponibles);
+
+}
+
+function deleteExtras() {
+let choosenNumber=document.getElementById("number").value;
+
+if (choosenNumber<=extraDisponibles.length) {
+  extraDisponibles.splice(choosenNumber,1);
+  mostrarExtras();
+  alert("Elemento borrado");
+} 
+
+else {
+  alert("La posición elegida no existe en el array");
+}
+
+}
+
+function addExtratoCar() {
+  //TODO
 }
