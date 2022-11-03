d6647b9e5c125152b325abfea5c487a25fa4c591
Anuska86 <anasrm86@gmail.com>
2022-11-03 11:57:14 +0100

    Base

--- actividad03.js
+++ actividad03.js
@@ -1,3 +1,5 @@
+//clase Extra con precio e imagen
+
 class Extra {
   constructor() {
     this.price = null;
@@ -7,6 +9,8 @@ class Extra {
     return `<span><img class="image" src='${this.url}' />${this.price}</span>`;
   }
 }
+
+//Creando extras 
 
 let extra = new Extra();
 extra.price = "17€";
@@ -39,6 +43,8 @@ class Coche {
   }
 }
 
+let extraDisponibles = [extra, extra2];
+
 let coche = new Coche();
 coche.name = "Tesla";
 coche.speed = "450km/h";
@@ -46,7 +52,12 @@ coche.addExtra(extra);
 coche.addExtra(extra2);
 console.log(coche.getHTML());
 
-let extraDisponibles = [extra, extra2];
+let coche2 = new Coche();
+coche2.name = "Volvo";
+coche2.speed = "350km/h";
+coche2.addExtra(extra);
+coche2.addExtra(extra2);
+console.log(coche2.getHTML());
 
 function mostrarExtras() {
   let result = "";
@@ -58,7 +69,25 @@ function mostrarExtras() {
 
 let cochesDisponibles = new Array();
 cochesDisponibles[coche.name] = coche;
+cochesDisponibles[coche2.name] = coche2;
 console.log(cochesDisponibles);
+
+function assignExtraToCar() {
+  let stringIni = '<label>Extra:</label><select id="extra" name="extra">';
+  for (let i = 0; i < extraDisponibles.length; i++) {
+    stringIni += `<option value="${i}">${i}</option>`;
+  }
+  stringIni += "</select>";
+  stringIni +=
+    '<label>Nombre del coche:</label><select id="carName" name="carName">';
+  let keys = Object.keys(cochesDisponibles);
+  for (let i = 0; i < keys.length; i++) {
+    let name = cochesDisponibles[keys[i]].name;
+    stringIni += `<option value="${name}">${name}</option>`;
+  }
+  stringIni += "</select>";
+  return stringIni;
+}
 
 function mostrarCoches() {
   let result = "";
@@ -71,40 +100,54 @@ function mostrarCoches() {
 
 function addExtras() {
   let extra = new Extra();
-  extra.price = document.getElementById("price").value;
+  extra.price = document.getElementById("price").value+" €";
   extra.url = "./imgs/" + document.getElementById("images").value + ".jpg";
 
   extraDisponibles.push(extra);
-  mostrarExtras();
+  const span = document.createElement("span");
+  const node = document.createElement("img");
+  node.class="image";
+  node.src = extra.url;
+  node.style = "  padding: 5px;width: 150px;height: 150px;"
+  span.innerHTML = extra.price;
+  span.appendChild(node);
+  document.getElementById("extrasPrueba").appendChild(span);
   console.log(extraDisponibles);
 }
 
-function addCar(){
+function addCar() {
   let coche = new Coche();
-  coche.name =document.getElementById("name").value;
+  coche.name = document.getElementById("name").value;
   coche.speed = document.getElementById("velocity").value;
 
-  cochesDisponibles[coche.name]=coche;
-  mostrarCoches();
+  cochesDisponibles[coche.name] = coche;
+  const spanCar =  document.createElement("span");
+  spanCar.innerHTML = coche.name+ " " + coche.speed+ " [ ]";
+  document.getElementById("carsPrueba").appendChild(spanCar);
   console.log(cochesDisponibles);
-
 }
 
 function deleteExtras() {
-let choosenNumber=document.getElementById("number").value;
+  let choosenNumber = document.getElementById("number").value;
 
-if (choosenNumber<=extraDisponibles.length) {
-  extraDisponibles.splice(choosenNumber,1);
-  mostrarExtras();
-  alert("Elemento borrado");
-} 
+  if (choosenNumber <= extraDisponibles.length) {
+    extraDisponibles.splice(choosenNumber, 1);
+    mostrarExtras();
+    alert("Elemento borrado");
+  } else {
+    alert("La posición elegida no existe en el array");
+  }
 
-else {
-  alert("La posición elegida no existe en el array");
-}
-
+  //$("#extrasPrueba").children().slice(0).detach();
+  //https://stackoverflow.com/questions/3400135/removing-children-elements-given-an-index
 }
 
 function addExtratoCar() {
-  //TODO
+  let choosenExtra = parseInt(document.getElementById("extra").value);
+  let choosenCar = document.getElementById("carName").value;
+
+  let extraObject = extraDisponibles[choosenExtra];
+  let carObject = cochesDisponibles[choosenCar];
+
+  carObject.extras.push(extraObject);
 }
